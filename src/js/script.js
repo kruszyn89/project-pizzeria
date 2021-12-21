c08bfdd8632a47ff56e59bd1bcaff5eda887409b
Dawid Kruszyński <dawid.kruszynski89@gmail.com>
2021-12-21 08:57:28 +0100

    init of  prepareCartProductParams method

--- src/js/script.js
+++ src/js/script.js
@@ -199,7 +199,7 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-
+      
       // set price to default price
       let price = thisProduct.data.price;
 
@@ -207,7 +207,7 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-
+        
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
@@ -216,6 +216,7 @@
           const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
           // setting the const in order to check if there is param with a name of paramId in formData and if it includes optionId
           const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          console.log();
 
           if(optionImage){
             if(optionSelected){
@@ -276,8 +277,63 @@
     }
     
     prepareCartProductParams(){
+      const thisProduct = this;
 
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      
+      // set price to default price
+      let price = thisProduct.data.price;
 
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          // find image with class .paramId-optionId
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          // setting the const in order to check if there is param with a name of paramId in formData and if it includes optionId
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          console.log();
+
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.wrapperActive);
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.wrapperActive);
+            }
+          }
+          // check if there is param with a name of paramId in formData and if it includes optionId 
+          if(optionSelected) {
+            // check if the option is not default
+            if(option.default !== true) {
+              // add option price to price variable
+              price += option.price;
+            }
+          } else {
+            // check if the option is default
+            if(option.default == true) {
+              // reduce price variable
+              price -= option.price;
+            }
+          }
+        }
+
+      }
+
+      /* Multiply price by amount */
+      price *= thisProduct.amountWidget.value;
+       
+      // update calculated price in the HTML
+      thisProduct.priceSingle = price;
+      
+      thisProduct.priceElem.innerHTML = price;
+    }
     }
 
   }
